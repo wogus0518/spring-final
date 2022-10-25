16bf27d4e12b636c3333740e0666089296e41278
JaeHyun <wogus0518@naver.com>
2022-10-25 10:44:50 +0900

    first commit

---
+++ src/main/java/codelion/springfinal/SpringFinalApplication.java
@@ -0,0 +1,13 @@
+package codelion.springfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFinalApplication.class, args);
+    }
+
+}
